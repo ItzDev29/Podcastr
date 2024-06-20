b15608877b23d8e354c762a95aa081b6efccf155
ItzDev29 <devmmehta2003@gmail.com>
2024-06-20 15:56:32 +0530

    Updated Developed line

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -72,10 +72,10 @@ const MobileNav = () => {
                 })}
                 <div className="flex mt-auto">
                   <SignedOut>
-                    <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
+                    <div className="flex-center flex-col w-full pb-14 max-lg:px-4 lg:pr-8">
                       <Button
                         asChild
-                        className="text-16 w-full bg-orange-1 font-extrabold"
+                        className="text-12 w-full bg-orange-1 font-extrabold"
                       >
                         <Link href="/sign-in">Sign in</Link>
                       </Button>
@@ -87,7 +87,7 @@ const MobileNav = () => {
                   <SignedIn>
                     <div className="flex-center flex-col w-full pb-10 max-lg:px-4 lg:pr-8">
                       <Button
-                        className="text-16 w-full bg-orange-1 font-extrabold"
+                        className="text-12 w-full bg-orange-1 font-extrabold"
                         onClick={() => signOut(() => router.push("/"))}
                       >
                         Log Out
